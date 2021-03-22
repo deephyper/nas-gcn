50a6e7a3417a6f010613ec575c0bd2f741700c65
Deathn0t <romainegele@gmail.com>
2021-03-22 08:42:04 +0100

    updated setup.py and .gitignore

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup, find_packages
 
 # What packages are required for this module to be executed?
-REQUIRED = [
-    "scikit-learn==0.21.3",
-    "deepchem==2.4.0rc1.dev20200819015415"
-]
-setup(
-    name='nas_gcn',
-    packages=find_packages(),
-    install_requires=REQUIRED
-)
+REQUIRED = ["scikit-learn", "deepchem>=2.4.0"]
+setup(name="nas_gcn", packages=find_packages(), install_requires=REQUIRED)
